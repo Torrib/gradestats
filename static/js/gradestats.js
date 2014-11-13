18569f3932851a0009a7d61a1e521ae622a7cc32
Håvard Lian <haavarhl@stud,ntnu,no>
2014-11-13 18:41:26 +0100

    Always print labels as integers

--- static/js/gradestats.js
+++ static/js/gradestats.js
@@ -27,7 +27,7 @@ $(function() {
 	        seriesDefaults:
 	        {
 	            renderer:$.jqplot.BarRenderer,
-	            pointLabels: { show: true },
+	            pointLabels: { show: true, formatString: '%d', formatter: $.jqplot.DefaultTickFormatter},
 	            rendererOptions: { barMargin: 2, varyBarColor: true},
 	        },
 	        axes:
