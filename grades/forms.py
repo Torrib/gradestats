00073d82b5465509aa86fc2e63c8f2903e4decf5
Håvard Lian <haavarhl@stud.ntnu.no>
2014-11-14 16:20:10 +0100

    Regexes now handles the entire norwegian alphabet :)

--- grades/forms.py
+++ grades/forms.py
@@ -16,7 +16,7 @@ class ReportErrorForm(forms.Form):
         label="Fagkode",
         max_length=16,
         validators=[RegexValidator(
-                    regex=r'^[a-zA-Z\xc5K]{2,5}[\d]{2,4}$',
+                    regex=r'^[a-zA-Z\xc6\xd8\xc5\xf8\xe6\xe5]{2,5}[\d]{2,4}$',
                     message="Invalid course code",
                     code='invalid_course_code')])
     semester_code = forms.CharField(
