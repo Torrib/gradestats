9aafc9e647f45f49e9e7edac9d3a2929c2d27193
oleast <oleast@stud.ntnu.no>
2020-04-28 15:32:51 +0200

    url -> path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 python3 manage.py migrate
-
+python3 manage.py buildwatson
 
 echo Starting uwsgi.
 exec uwsgi --chdir=/srv/app \
--- gradestats/urls.py
+++ gradestats/urls.py
@@ -1,5 +1,7 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.contrib import admin
+from django.urls import path
+
 from grades.views import *
 from grades_api.views import *
 from rest_framework.urlpatterns import format_suffix_patterns
@@ -9,42 +11,26 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    url(r"^$", index, name="index"),
-    url(r"^index/$", index, name="index"),
-    url(r"^course/$", index, name="index"),
-    url(
-        r"^course/(?P<course_code>[a-zA-Z\xc6\xd8\xc5\xf8\xe6\xe5]{1,8}[\d]{2,6})/$",
-        course,
-        name="course",
-    ),
-    url(
-        r"^course/(?P<course_code>[a-zA-Z\xc6\xd8\xc5\xf8\xe6\xe5]{1,8}[\d]{2,6})/tags/add/$",
-        add_tag,
-        name="add_tag",
-    ),
-    url(
-        r"^course/(?P<course_code>[a-zA-Z\xc6\xd8\xc5\xf8\xe6\xe5]{1,8}[\d]{2,6})/grades/$",
-        get_grades,
-        name="get_grades",
-    ),
-    url(r"^search/$", search, name="search"),
-    url(r"^about/$", about, name="about"),
-    url(r"^report/$", report, name="report"),
-    url(r"^api/$", api, name="api"),
-    url(r"^admin/", admin.site.urls),
+    path("", index, name="index"),
+    path("index/", index, name="index"),
+    path("course/", index, name="index"),
+    path("course/<course_code>/", course, name="course",),
+    path("course/<course_code>/tags/add/", add_tag, name="add_tag",),
+    path("course/<course_code>/grades/", get_grades, name="get_grades",),
+    path("search/", search, name="search"),
+    path("about/", about, name="about"),
+    path("report/", report, name="report"),
+    path("api/", api, name="api"),
+    path("admin/", admin.site.urls),
 ]
 
 router = SimpleRouter(trailing_slash=False)
 router.register("api/courses", CourseViewSet)  # Create routes for Courses
-## No regex on urlpattern
-# regex = router.get_urls()[1].regex.pattern[1:-1]
 router.register(
-    "api/courses/(?P<course_code>[a-zA-Z\xc6\xd8\xc5\xf8\xe6\xe5]{1,8}[\d]{2,6})/grades",
-    GradeViewSet,
-)  # Create routes for grades using regex from course route as base
+    "api/courses/<course_code>/grades", GradeViewSet,
+)
 router.register(
-    "api/courses/(?P<course_code>[a-zA-Z\xc6\xd8\xc5\xf8\xe6\xe5]{1,8}[\d]{2,6})/grades/",
-    GradeViewSet,
+    "api/courses/<course_code>/grades/", GradeViewSet,
 )
 router.register("api/index", CourseIndexViewSet)
 router.register("api/typeahead/course", CourseTypeaheadViewSet)
@@ -53,7 +39,7 @@ urlpatterns += format_suffix_patterns(router.urls, allowed=["json"])
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if "grades_api_v2" in settings.INSTALLED_APPS:
-    urlpatterns += [url(r"^api/v2/", include("grades_api_v2.urls"))]
+    urlpatterns += [path("api/v2/", include("grades_api_v2.urls"))]
 
 if "rest_framework" in settings.INSTALLED_APPS:
     from grades_api_v2.router import SharedAPIRootRouter
@@ -61,4 +47,4 @@ if "rest_framework" in settings.INSTALLED_APPS:
     def api_urls():
         return SharedAPIRootRouter.shared_router.urls
 
-    urlpatterns += [url(r"^api/v2/", include(api_urls()))]
+    urlpatterns += [path("api/v2/", include(api_urls()))]
