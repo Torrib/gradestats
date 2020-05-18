437989afacf31675415e44a55d7f92bf7e7fa16f
oleast <oleast@stud.ntnu.no>
2020-05-18 08:37:57 +0200

    Set correct permissions for NSDclient API

--- grades_api_v2/views.py
+++ grades_api_v2/views.py
@@ -204,7 +204,7 @@ class NSDScraperViewSet(viewsets.GenericViewSet):
         detail=False,
         methods=["POST"],
         serializer_class=NSDGradeReportSerializer,
-        permission_classes=(permissions.AllowAny,),
+        permission_classes=(permissions.IsAdminUser,),
     )
     def grade_report(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
